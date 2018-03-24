42ed1fe4152aad9dfa1a022b544093ce775ff94e
Prapojjanaporn, Pruet (Agoda) <pprapojjanap@c02th23yfvh4.agoda.local>
2018-03-24 14:28:53 +0700

    Refactor

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,34 +2,28 @@ import java.nio.charset.StandardCharsets
 import java.util
 
 import org.apache.kafka.clients.consumer.KafkaConsumer
-import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+import org.apache.kafka.clients.producer.KafkaProducer
 import scaldi.Injectable
 
 object Main extends App with Injectable{
 
   import bindings.Binding.appInjector
 
+  private val topicName = "test"
+
   val producer = inject[KafkaProducer[Integer,Array[Byte]]]
-  val data = ProducerRecordTest("test", "This is a message")
+  val data = ProducerRecordTest(topicName, "This is a message")
   producer.send(data)
   producer.close()
 
   val consumer = inject[KafkaConsumer[Integer,Array[Byte]]]
-  consumer.subscribe(util.Arrays.asList("test"))
+  consumer.subscribe(util.Arrays.asList(topicName))
   val records = consumer.poll(5000)
   println(records.count)
-  val count = records.count
   val recordIterator = records.iterator
-  var i = 0
-  while ( {
-    i < count
-  }) {
+  while ( recordIterator.hasNext) {
     val record = recordIterator.next
     val output = new String(record.value, StandardCharsets.UTF_8)
     println(output)
-
-    {
-      i += 1; i - 1
-    }
   }
 }
